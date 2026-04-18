a0e9d83a71a1cdb95fec8059867c11618cf9932c
RohitSKoujalagi <rohitkaujalgi@gmail.com>
2026-04-18 14:23:52 +0530

    Migrate from Flask to Django for request handling
    Replaced Flask app with Django view to handle requests.

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,21 +1,4 @@
-from flask import Flask
-from prometheus_client import make_wsgi_app, Counter
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from django.http import HttpResponse
 
-app = Flask(__name__)
-
-# Add a simple metric
-REQUESTS = Counter('flask_app_requests_total', 'Total number of requests')
-
-@app.route('/')
-def hello():
-    REQUESTS.inc()
-    return "Hello World!"
-
-# Add the prometheus wsgi middleware to route /metrics requests
-app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
-    '/metrics': make_wsgi_app()
-})
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+def index(request):
+ return HttpResponse("Namaste, Duniya!\n") 
